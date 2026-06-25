0f8f5c7b4539a0a8fcc5746276dcd6a55d0a043a
Pedro Lameira <120721002+lameiragaspar@users.noreply.github.com>
2026-06-25 13:17:09 +0100

    Arquivos inúteis deletados

--- arquivos/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  allowedDevOrigins: ['192.168.100.10'],
-  //allowedDevOrigins: ['192.168.10.84'],
-  images: {
-    domains: ['exemplo.com', 'cdn.exemplo.com'],
-  },
-};
--- arquivos/supabase-admin.js
+++
@@ -1,7 +0,0 @@
-// lib/supabase-admin.js  — apenas para uso em API routes
-import { createClient } from '@supabase/supabase-js'
-
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
-)
